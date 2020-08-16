e74b784213e99176ef2ea37a7e4106f33fa039df
pangosol95 <pangosol2538@gmail.com>
2020-08-16 12:29:13 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Profile from "./components/Profile";
+import About from "./components/About";
+import Products from "./components/Products";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about"  component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/products" component={Products} />
+    <Footer />
+
+
+    </div>
+  );
+}
+
+export default App;
