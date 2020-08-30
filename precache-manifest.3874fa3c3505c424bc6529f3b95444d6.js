204e324c5333db989c8ea564029d9e3462af9d1a
pangosol95 <pangosol2538@gmail.com>
2020-08-30 12:10:18 +0700

    Updates

---
+++ precache-manifest.3874fa3c3505c424bc6529f3b95444d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b59e84da525dcfe07e5decabd1ad457",
+    "url": "/busines-web/index.html"
+  },
+  {
+    "revision": "0a131cd304a40c2f8632",
+    "url": "/busines-web/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e60b42dbaf60c9c7cb6f",
+    "url": "/busines-web/static/css/main.b6549f76.chunk.css"
+  },
+  {
+    "revision": "0a131cd304a40c2f8632",
+    "url": "/busines-web/static/js/2.5bb08ee9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/busines-web/static/js/2.5bb08ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60b42dbaf60c9c7cb6f",
+    "url": "/busines-web/static/js/main.7be9094e.chunk.js"
+  },
+  {
+    "revision": "b500a60060b7919d0670",
+    "url": "/busines-web/static/js/runtime-main.a0e88c51.js"
+  }
+]);
